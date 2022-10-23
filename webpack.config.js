0fba9307cefb1c52c78356143c75387cd9e2080e
Devin DeJongh <devindejongh@gmail.com>
2022-10-22 22:15:41 -0500

    this isn't working lol

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+
+    // Entry point that indicates where
+    // the webpack should start bundling
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+    // Options for resolving module requests
+    // extensions that are used
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+
+    // Output point where webpack should
+    // output the bundles and assets
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js",
+    },
+};
